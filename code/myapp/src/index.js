3fc542ffd91ce8bd63177ba2ff8ebc85c5a8d0fd
Yangyukai0129 <beir6108@gmail.com>
2023-11-19 19:12:11 +0800

    增加09練習

--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 
 // createRoot(document.getElementById('root')).render(<div>234</div>);
 
-import App from './02-advanced/08-訂閱發布模式案例'
+import App from './02-advanced/09-context'
 createRoot(document.getElementById('root')).render(<App></App>)
 
 // jsx == js + xml
