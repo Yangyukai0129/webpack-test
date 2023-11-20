8f8bf8d42df62204826326cbcd77adf1327c31d8
kevin_yang <kevin_yang@comwave.com.tw>
2023-11-20 09:09:07 +0800

    all

--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 
 // createRoot(document.getElementById('root')).render(<div>234</div>);
 
-import App from './02-advanced/06-中間人模式'
+import App from './02-advanced/04-父子通信版-表單域組件'
 createRoot(document.getElementById('root')).render(<App></App>)
 
 // jsx == js + xml
