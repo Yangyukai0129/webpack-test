e0b916c60babb8fc18783f6e4ec39efde19951d5
Yangyukai0129 <beir6108@gmail.com>
2023-11-24 21:44:05 +0800

    增加18練習

--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 
 // createRoot(document.getElementById('root')).render(<div>234</div>);
 
-import App from './02-advanced/17-生命週期-更新階段3-案例'
+import App from './02-advanced/18-生命週期-銷毀'
 
 createRoot(document.getElementById('root')).render(<App></App>)
 
