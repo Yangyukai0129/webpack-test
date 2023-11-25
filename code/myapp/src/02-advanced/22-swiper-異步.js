9d29ad8cb0b49e6ea72591dd657e93faaf0a308c
Yangyukai0129 <beir6108@gmail.com>
2023-11-25 19:13:42 +0800

    更新22

--- code/myapp/src/02-advanced/22-swiper-異步.js
+++ code/myapp/src/02-advanced/22-swiper-異步.js
@@ -32,10 +32,10 @@ export default class App extends Component {
 
     componentDidMount() {
         setTimeout(() => {
-            // react18後將setState變異步了，可以在setState傳第二個參數-回調函數，在回調函數進行初始化
             this.setState({
                 list: ["111", "222", "333"]
             },
+                // react18後將setState變異步了，可以在setState傳第二個參數-回調函數，在回調函數進行初始化
                 () => {
                     // 如果使用 initSwiper()，initSwiper 函数内的 this 将不再指向组件的实例，而是指向全局对象（在浏览器中通常是 window 对象）。这会导致在 initSwiper 函数中无法正确访问组件的实例，也无法访问组件的状态、属性等。
                     // 为了确保在函数内部能够正确访问组件实例，需要使用 this.initSwiper()。这样，在函数内部的 this 将指向当前组件的实例，使得函数能够正确访问组件的属性、状态等信息。
