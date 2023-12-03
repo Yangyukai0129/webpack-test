feee9ed30e6f3cc04d454f5b6890d8bba3fd15a7
Yangyukai0129 <beir6108@gmail.com>
2023-12-03 22:09:10 +0800

    安裝react-router，練習react-router

--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import { ReactDOM } from 'react';
-import { createRoot } from 'react-dom/client'
+// import React from 'react';
+// import { ReactDOM } from 'react';
+// import { createRoot } from 'react-dom/client'
 
-// createRoot(document.getElementById('root')).render(<div>234</div>);
+// // createRoot(document.getElementById('root')).render(<div>234</div>);
 
-import App from './03-hooks/14-自訂義hooks'
+// import App from './03-hooks/14-自訂義hooks'
 
-createRoot(document.getElementById('root')).render(<App></App>)
+// createRoot(document.getElementById('root')).render(<App></App>)
 
-// react18里面已经弃用了ReactDOM.render,不过浏览器依旧能正常运行ReactDOM.render，react18里用下面方法构建dom。
-// import {createRoot} from "react-dom/client";
+// // react18里面已经弃用了ReactDOM.render,不过浏览器依旧能正常运行ReactDOM.render，react18里用下面方法构建dom。
+// // import {createRoot} from "react-dom/client";
 
-// const container=document.getElementById("root")
-// const root=createRoot(container)
-// root.render(<Element></Element>)
-// jsx == js + xml
+// // const container=document.getElementById("root")
+// // const root=createRoot(container)
+// // root.render(<Element></Element>)
+// // jsx == js + xml
+
+
+//練習React route 
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./04-route/index.css";
+import Root from "./04-route/routes/root"
+import ErrorPage from "./04-route/error-page";
+import Contact from "./04-route/contact";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        Children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
